103c3ca2ec8ee5cd3a7622b4d59bf19076eecd65
Maykon Fonseca <mykosistema@gmail.com>
2024-11-01 11:43:03 -0300

    salvamento pós desenvolvimento base back-office e adaptação banco de dados

---
+++ saboroso/routes/index.js
@@ -0,0 +1,21 @@
+var conn = require('./../inc/db');
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+
+  conn.query(`
+    SELECT * FROM tb_menus ORDER BY title
+    `, (err, results)=> {
+      if(err) {
+        console.log(err);
+      }
+      res.render('index', 
+        { title: 'Restaurante Saboroso!',
+          menus: results
+        });
+    });
+});
+
+module.exports = router;
