050151a732f5f45da0ca4416b06ec5a4355a9773
Maykon Fonseca <mykosistema@gmail.com>
2024-11-01 16:59:06 -0300

    salvamento pós desenvolvimento do backoffice, semi-completo

--- saboroso/routes/index.js
+++ saboroso/routes/index.js
@@ -1,21 +1,76 @@
 var conn = require('./../inc/db');
 var express = require('express');
+var menus = require('./../inc/menus');
+var reservations = require('./../inc/reservations');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  conn.query(`
-    SELECT * FROM tb_menus ORDER BY title
-    `, (err, results)=> {
-      if(err) {
-        console.log(err);
-      }
-      res.render('index', 
-        { title: 'Restaurante Saboroso!',
-          menus: results
-        });
+    menus.getMenus().then(results => {
+
+      res.render('index', {
+        title: 'Restaurante Saboroso!',
+        menus: results,
+        isHome: true,
+      });
     });
+});
+
+router.get('/contacts', function(req, res, next) {
+  
+  res.render('contacts', {
+    title: 'Contato - Restaurante Saboroso!',
+    background: "images/img_bg_3.jpg",
+    h1: "Diga um oi!",
+    isHome: false,
+  });
+}); 
+
+router.get('/menus', function(req, res, next) {
+
+  menus.getMenus().then(results => {
+    res.render('menus', {
+      title: 'Menus - Restaurante Saboroso!',
+      background: "images/img_bg_1.jpg",
+      h1: "Saboreie nosso menu!",
+      menus: results,
+      isHome: false
+    });
+  });
+}); 
+
+router.get('/reservations', function(req, res, next) {
+  reservations.render(req, res);
+}); 
+
+router.post('/reservations', function(req, res, next) {
+  if (!req.body.name) {
+    reservations.render(req, res, "Digite o nome" );
+  } else if (!req.body.email) {
+    reservations.render(req, res,"Digite o e-mail");
+  } else if (!req.body.people) {
+    reservations.render(req, res,"Selecione o número de pessoas");
+  } else if (!req.body.date) {
+    reservations.render(req, res,"Selecione a hora");
+  } else {
+    reservations.save(req.body).then(results => {
+      req.body = {}
+      reservations.render(req, res, null, "Reserva realizada com sucesso")
+    }).catch(err => {
+      reservations.render(req, res, err.message);
+    })
+  }
+});
+
+router.get('/services', function(req, res, next) {
+  
+  res.render('services', {
+    title: 'Serviços - Restaurante Saboroso!',
+    background: "images/img_bg_1.jpg",
+    h1: "É um prazer poder servir!",
+    isHome: false
+  });
 });
 
 module.exports = router;
